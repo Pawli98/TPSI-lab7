2498ca9ecbee461bb579cf2b689711468dc2ee08
Paweł Gruhn <50170421+Pawli98@users.noreply.github.com>
2020-04-21 15:37:27 +0200

    Add files via upload

---
+++ src/main/java/wizut/tpsi/Lab7Application.java
@@ -0,0 +1,14 @@
+package wizut.tpsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan(basePackages={"wizut.tpsi"})
+public class Lab7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7Application.class, args);
+	}
+}
